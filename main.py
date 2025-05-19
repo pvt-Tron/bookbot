8a1970b345216fb9715a7bc32b3b2cb98e29c9e3
pvt-Tron <nickpvt@yahoo.it>
2025-05-19 16:26:17 +0200

    Z: First Prjkt Complete 19-05-25

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+# !/usr/bin/python3
 import sys
 from stats import get_num_words
 from stats import get_num_chars
@@ -14,40 +14,39 @@ def main():
 	# sys.argv[1] = "books/"
 	arg1s = sys.orig_argv
 	# ls1 = len(sys.orig_argv)
-	ls1 = len(sys.argv)
+	# ls2 = len(sys.argv)
 	# print(ls1)
+	# print(ls2)
 	# print(arg1s)
-		
-	# arg2s = arg1s[2:]
-	i2 = 1
-	for i in range(1, ls1):
-		i2 += 1
-		# if(i < 2):
-		# 	continue
-		inptpth = sys.argv[i]
-	if i < 2:
+	la1 = len(arg1s)
+	# print(la1)
+	if la1 < 3:
 		print("Usage: python3 main.py <path_to_book>")
-		sys.exit(1)
+		sys.exit(1)	
+	arg2s = arg1s[2:] # # slice the 2 default agrs
+	# print(arg2s)
+	# for i in range(1, arg1s - 1):
+	for arg2 in arg2s:
+		inptpth = arg2
+		
+		# stpth1 = "~/wrksp/boot.dev/github.com/pvt-Tron/bookbot/"
+		# print(stpth1)
+		# inptpth = stpth1 + arg2
+	 	# inptpth = sys.argv[i] ## <<<<<<
+
 		# inptpth = "books/frankenstein.txt"
+		# ## books/frankenstein.txt mobydick.txt prideandprejudice.txt
+
 		print("============ BOOKBOT ============")
-		print(f"Analyzing book found at {inptpth}...") # books/frankenstein.txt
+		print(f"Analyzing book found at {inptpth}...") 
 		print("----------- Word Count ----------")
 		numwords = get_num_words(get_book_text(inptpth))
 		# print(f"{numwords} words found in the document")
 		print(f"Found {numwords} total words")
 		print("--------- Character Count -------")
 		cntchrdic = get_num_chars(get_book_text(inptpth))
-		# print(cntchrdic)
-		# print(repr(cntchrdic))
-		flag1 = 1
-		for i, j in cntchrdic.items():
-			# if flag1 == 1:
-			# 	flag1  = 0
-			# 	continue
-			# else:
-			print(f"{i}: {j}")
 
-		# prnt_lst(cntchrdic)
-	
+		for i, j in cntchrdic.items():
+			print(f"{i}: {j}")	
 main()
 
